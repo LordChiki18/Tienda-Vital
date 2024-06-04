a1da81bcc9bf127ad04562861bad44dd7f2f68c1
LordChiki18 <cristiandomecqc@gmail.com>
2024-06-04 12:04:41 -0400

    Test 1.1

--- tienda/settings.py
+++ tienda/settings.py
@@ -131,7 +131,7 @@ WSGI_APPLICATION = 'tienda.wsgi.app'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
